0d4fc7603d2c5e98024f24aacea58dc418bed1d5
idarbandi <darbandidr99@gmail.com>
2025-01-06 20:12:27 +0330

    added store admin

--- store/admin.py
+++ store/admin.py
@@ -1,40 +1,40 @@
-# from django.contrib import admin
-# from mptt.admin import MPTTModelAdmin
+from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
-# from .models import (
-#     Category,
-#     Product,
-#     ProductImage,
-#     ProductSpecification,
-#     ProductSpecificationValue,
-#     ProductType,
-# )
+from .models import (
+    Category,
+    Product,
+    ProductImage,
+    ProductSpecification,
+    ProductSpecificationValue,
+    ProductType,
+)
 
-# admin.site.register(Category, MPTTModelAdmin)
-
-
-# class ProductSpecificationInline(admin.TabularInline):
-#     model = ProductSpecification
+admin.site.register(Category, MPTTModelAdmin)
 
 
-# @admin.register(ProductType)
-# class ProductTypeAdmin(admin.ModelAdmin):
-#     inlines = [
-#         ProductSpecificationInline,
-#     ]
+class ProductSpecificationInline(admin.TabularInline):
+    model = ProductSpecification
 
 
-# class ProductImageInline(admin.TabularInline):
-#     model = ProductImage
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductSpecificationInline,
+    ]
 
 
-# class ProductSpecificationValueInline(admin.StackedInline):
-#     model = ProductSpecificationValue
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
 
 
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-#     inlines = [
-#         ProductSpecificationValueInline,
-#         ProductImageInline
-#     ]
+class ProductSpecificationValueInline(admin.StackedInline):
+    model = ProductSpecificationValue
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductSpecificationValueInline,
+        ProductImageInline
+    ]
