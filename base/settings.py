afcef1f407cb1891f917c1249af9c907ee1f69dc
idarbandi <darbandidr99@gmail.com>
2025-01-07 20:04:16 +0330

    added store urls

--- base/settings.py
+++ base/settings.py
@@ -21,7 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
-    'mptt'
+    'mptt',
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -103,8 +104,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
